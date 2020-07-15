aa903c7cb9b9f6d6d63a14f0f9109448ca761c44
rsmith013 <richard.d.smith@stfc.ac.uk>
2020-07-15 17:27:01 +0100

    more output

--- create_dataset.py
+++ create_dataset.py
@@ -5,20 +5,15 @@ import json
 all_lines = []
 total = 0
 
-for file in os.listdir('../moles-access-logs'):
-    with open(os.path.join('../moles-access-logs', file)) as reader:
+for file in os.listdir('input_logs'):
+    print(file)
+    with open(os.path.join('input_logs', file)) as reader:
         lines = reader.readlines()
 
     output = []
 
-    pattern = re.compile(r'^(?P<ip>[0-9.]+)\s'
-                         r'(?P<identity>[\w.-]+)\s'
-                         r'(?P<userid>[\w.-]+)\s'
-                         r'\[(?P<date>.+)]\s'
-                         r'\"(?P<request>GET[\w\s/\-\.?=&,+%*\']+)\"\s'
-                         r'(?P<status_code>\d{3})\s'
-                         r'(?P<response_bytes>\d+|-)\s'
-                         r'\"(?P<referer>[\w\s/\-\.?=&:,%+*\']+)\"')
+    pattern = re.compile(
+        r'^(?P<ip>[0-9.]+)\s(?P<identity>[\w.-]+)\s(?P<userid>[\w.-]+)\s\[(?P<date>.+)]\s\"(?P<request>GET[\w\s/\-\.?=&,+%*\']+)\"\s(?P<status_code>\d{3})\s(?P<response_bytes>\d+|-)\s\"(?P<referer>[\w\s/\-\.?=&:,%+*\']+)\"')
 
     # Filter lines
     for line in lines:
@@ -30,19 +25,19 @@ for file in os.listdir('../moles-access-logs'):
 
             m = pattern.search(line)
             if m and m.group('referer') != '-':
-                if 'uuid' not in m.group('request') or 'http://catalogue.ceda.ac.uk/?q' not in m.group('referer'): continue
+
+                if 'uuid' not in m.group('request') or 'https://catalogue.ceda.ac.uk/?q' not in m.group('referer'): continue
                 output.append(json.dumps({
-                    'ip': m.group('ip'),
+                    # 'ip': m.group('ip'),
                     'date': m.group('date'),
                     'request': m.group('request'),
                     'referer': m.group('referer')
-                })+'\n')
-
+                }) + '\n')
 
     total += len(lines)
     all_lines.extend(output)
 
-print 'Total input: {} Output lines: {} Shrink: {}%'.format(total, len(all_lines),
-                                                            100 - round((float(len(all_lines)) / total) * 100, 3))
-with open('output_logs.txt', 'w') as writer:
+print('Total input: {} Output lines: {} Shrink: {}%'.format(total, len(all_lines),
+                                                            100 - round((float(len(all_lines)) / total) * 100, 3)))
+with open('output_logs1.txt', 'w') as writer:
     writer.writelines(all_lines)
